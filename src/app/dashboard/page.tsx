d9eee5d70310a01bf96381d981dc7659967ca6a8
elchakal23 <moneyemoji.pro@gmail.com>
2023-09-22 18:52:49 +0200

    2nd changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -23,7 +23,7 @@ const Dasboard = async (props: Props) => {
   return (
     <main className="p-8 mx-auto max-w-7xl">
       <div className="flex items-center">
-        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboarda</h2>
         <DetailsDialog />
       </div>
 
